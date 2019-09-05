a024be418ac314eaaa43c2e7e1019520585eed0f
karan singh <thekrsingh@gmail.com>
2019-09-05 13:33:58 +0530

    base upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function learnMore() {
+  document.querySelector("#box2").scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+function skills() {
+  document.querySelector("#box4").scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
